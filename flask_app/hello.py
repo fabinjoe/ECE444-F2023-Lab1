b48c4ab5df1433f49e8ef2c6592babc270c6ab8d
Fabin Joe <fabin2005@outlook.com>
2023-09-19 23:39:05 -0400

    Ex 4.7: Added a Form with Name field

--- flask_app/hello.py
+++ flask_app/hello.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4829jfnwurduh4293k'
 bootstrap = Bootstrap(app=app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print(datetime.utcnow())
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+    form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
